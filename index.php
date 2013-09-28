81386accc5a9637d16049d01f5f0d33d363fb56b
system1 <system1@system1-PC>
2013-09-28 11:26:06 -0700

    this is my first version

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of index
+ *
+ * @author system1
+ */
+class index {
+    //put your code here
+}
+
+?>
